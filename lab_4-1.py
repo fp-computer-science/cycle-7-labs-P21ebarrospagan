d9f5401db09fff8cc65a7844f3b6ba37109e863b
Eugene Barros-Pagan <p21ebarrospagan@fairfieldprep.org>
2020-11-06 13:20:55 -0500

    Lab(s) 3

---
+++ lab_4-1.py
@@ -0,0 +1,7 @@
+# Author: EBP (AMDG) 11/6/20 
+
+fstring = "flibbertgibibett" 
+print(fstring.find('t'))
+
+sstring = "I wish, I wish, I was a fish." 
+print(sstring.split(','))
---
+++ lab_4-2.py
@@ -0,0 +1,6 @@
+# Author: EBP (AMDG) 11/6/2020
+
+name1 = input("enter you name: ")
+name2 = input("enter the other person's name: ")
+
+print("Hello, {0}. My name is {1}.". format(name1, name2))
